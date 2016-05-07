38e75777695e3aeb24d9f78b69d3cef88abe2cf5
Nick Stephens <nick.d.stephens@gmail.com>
2016-05-06 21:16:45 -0700

    Properly make stdin concrete

--- colorguard/colorguard.py
+++ colorguard/colorguard.py
@@ -1,4 +1,8 @@
 import tracer
+import claripy
+from simuvex.plugins.symbolic_memory import SimSymbolicMemory
+from simuvex.storage import SimFile
+
 from .simprocedures import ColorGuardTransmitHook, FlagLeakDetected
 
 class ColorGuard(object):
@@ -15,8 +19,15 @@ class ColorGuard(object):
 
         self._tracer = tracer.Tracer(binary, icontent, simprocedures={'transmit': ColorGuardTransmitHook})
 
-        __import__("ipdb").set_trace()
-        self._tracer.path_group
+
+        # fix up the tracer so that it the input is completely concrete
+        e_path = self._tracer.path_group.active[0]
+
+        backing = SimSymbolicMemory(memory_id='file_colorguard')
+        backing.set_state(e_path.state)
+        backing.store(0, e_path.state.se.BVV(icontent))
+
+        e_path.state.posix.files[0] = SimFile('/dev/stdin', 'r', content=backing, size=len(icontent))
 
     def causes_leak(self):
 
