f1697783a48218ba0b6ef2b00a74b406ebda284d
Nick Stephens <nick.d.stephens@gmail.com>
2016-05-12 22:07:33 -0700

    Colorguard now completes the trace and analyzes stdout to get leaked bytes

--- colorguard/colorguard.py
+++ colorguard/colorguard.py
@@ -3,8 +3,6 @@ import harvester
 from simuvex.plugins.symbolic_memory import SimSymbolicMemory
 from simuvex.storage import SimFile
 
-from .simprocedures import ColorGuardTransmitHook, FlagLeakDetected
-
 class ColorGuard(object):
     """
     Detect leaks of the magic flag page data.
@@ -17,8 +15,7 @@ class ColorGuard(object):
         :param icontent: concrete input string to feed to the binary
         """
 
-        self._tracer = tracer.Tracer(binary, icontent, preconstrain=False, simprocedures={'transmit': ColorGuardTransmitHook})
-
+        self._tracer = tracer.Tracer(binary, icontent, preconstrain=False)
 
         # fix up the tracer so that it the input is completely concrete
         e_path = self._tracer.path_group.active[0]
@@ -28,31 +25,41 @@ class ColorGuard(object):
         backing.store(0, e_path.state.se.BVV(icontent))
 
         e_path.state.posix.files[0] = SimFile('/dev/stdin', 'r', content=backing, size=len(icontent))
+        e_path.state._colorguard = self
 
-        self.sym_bytes = [ ]
+        self.leak_ast = None
 
     def causes_leak(self):
 
-        try:
-            self._tracer.run()
-        except FlagLeakDetected as fld:
-            self.sym_bytes.append(fld.args[0])
-            return True
+        path, _ = self._tracer.run()
+
+        stdout = path.state.posix.files[1]
+
+        tmp_pos = stdout.read_pos
+        stdout.pos = 0
+
+        output = stdout.read_from(tmp_pos)
+
+        for var in output.variables:
+            if var.split("_")[0] == "cgc-flag-data":
+                self.leak_ast = output
+                return True
 
         return False
 
     def attempt_leak(self):
 
-        assert len(self.sym_bytes) > 0, "run causes_leak before attempting to exploit"
-
-        # TODO: detect if we have four contiguous bytes
-
-        sym_bytes = self.sym_bytes[0]
+        assert self.leak_ast is not None, "must run causes_leak first or input must cause a leak"
 
         # convert to C code
-        h = harvester.Harvester(sym_bytes)
-        h.reverse()
 
-        code = h.to_c()
+        print self.leak_ast
+        h = harvester.Harvester(self.leak_ast)
+        result = h.reverse()
+
+        print result.leaked_bytes()
+
+        code = h.to_c(result)
 
         # TODO: make into a pov
+        return code
