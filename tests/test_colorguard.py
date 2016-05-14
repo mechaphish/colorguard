5d1fd755c56a8f0c602b2a86192384bf9c3f7823
Nick Stephens <nick.d.stephens@gmail.com>
2016-05-13 20:34:54 -0700

    Add new tests

--- tests/test_colorguard.py
+++ tests/test_colorguard.py
@@ -5,14 +5,49 @@ import os
 
 bin_location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../binaries-private'))
 
-def test_simple_leak():
+def test_simple_leak1():
     """
     Test detection of one of the simplest possible leaks.
     """
 
-    cg = colorguard.ColorGuard(os.path.join(bin_location, 'tests/i386/simple_leak'), 'foobar')
+    cg = colorguard.ColorGuard(os.path.join(bin_location, 'tests/i386/simple_leak1'), 'foobar')
 
     nose.tools.assert_true(cg.causes_leak())
+    pov = cg.attempt_pov()
+    nose.tools.assert_true(pov.test_binary())
+
+def test_simple_leak2():
+    """
+    Test detection of a leak where multiple arithmetic operations are performed on flag page data.
+    """
+
+    cg = colorguard.ColorGuard(os.path.join(bin_location, 'tests/i386/simple_leak2'), 'foobar')
+
+    nose.tools.assert_true(cg.causes_leak())
+    pov = cg.attempt_pov()
+    nose.tools.assert_true(pov.test_binary())
+
+def test_simple_leak3():
+    """
+    Test detection of a leak where bytes leaked through different calls to transmit.
+    """
+
+    cg = colorguard.ColorGuard(os.path.join(bin_location, 'tests/i386/simple_leak3'), 'foobar')
+
+    nose.tools.assert_true(cg.causes_leak())
+    pov = cg.attempt_pov()
+    nose.tools.assert_true(pov.test_binary())
+
+def test_simple_leak4():
+    """
+    Test detection of a leak where bytes leaked through different calls to transmit and operations are done to those bytes.
+    """
+
+    cg = colorguard.ColorGuard(os.path.join(bin_location, 'tests/i386/simple_leak4'), 'foobar')
+
+    nose.tools.assert_true(cg.causes_leak())
+    pov = cg.attempt_pov()
+    nose.tools.assert_true(pov.test_binary())
 
 def run_all():
     functions = globals()
