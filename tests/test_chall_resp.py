42c7f7d200b18626cede42ee92ffa47a5763f6b9
Nick Stephens <nick.d.stephens@gmail.com>
2016-07-22 04:16:04 -0700

    Update chall resp testcase with correct flag info

--- tests/test_chall_resp.py
+++ tests/test_chall_resp.py
@@ -10,7 +10,7 @@ logging.getLogger("colorguard").setLevel("DEBUG")
 logging.getLogger("povsim").setLevel("DEBUG")
 
 def test_simple_chall_resp():
-    cg = colorguard.ColorGuard(os.path.join(bin_location, "tests/i386/CUSTM_00022"), '\xf0EY\xcaAAA')
+    cg = colorguard.ColorGuard(os.path.join(bin_location, "tests/i386/CUSTM_00022"), '\xa0\x9d\x9a\x35AA')
 
     nose.tools.assert_true(cg.causes_leak())
     pov = cg.attempt_pov()
